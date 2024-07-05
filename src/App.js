3f388b836fd51b540d4ce8e571a9926915c0079d
DharaniMohanachandran <dharanim.21eie@kongu.edu>
2024-07-05 23:20:48 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { TodoWrapper } from './compenents/TodoWrapper';
+
+
+function App() {
+  return (
+    <div className="App">
+      <TodoWrapper/>
+    </div>
+  );
+}
+
+export default App;
